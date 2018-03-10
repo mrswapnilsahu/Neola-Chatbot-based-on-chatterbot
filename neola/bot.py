5d4d2e01a5cd8d824761beef26640f80bcefdbab
Anuj Rajak <mr.anujrajak@gmail.com>
2018-03-10 13:21:29 +0530

    Update bot.py

--- neola/bot.py
+++ neola/bot.py
@@ -1,9 +1,8 @@
 from chatterbot import ChatBot
 bot = ChatBot(
-    "neola",
+    "Neola",
     database="db.sqlite3"
 )
-
 while True:
     request = input('You : ')
     res = bot.get_response(request)
